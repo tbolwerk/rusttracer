68c29601eaa0a81822394bb6782644d7802ac9a2
Twan Bolwerk <twan.bolwerk@gridtogreat.com>
2026-01-01 12:00:51 +0100

    Remove println debug statement from translation

--- transformations.rs
+++ transformations.rs
@@ -24,10 +24,64 @@ pub const fn scaling(x: f32, y: f32, z: f32) -> Matrix<4, 4> {
     m
 }
 
+const fn cos(x: f32) -> f32 {
+    let x2 = x * x;
+    let x4 = x2 * x2;
+    let x6 = x4 * x2;
+    1.0 - x2 / 2.0 + x4 / 24.0 - x6 / 720.0
+}
+
+const fn sin(x: f32) -> f32 {
+    let x3 = x * x * x;
+    let x5 = x3 * x * x;
+    let x7 = x5 * x * x;
+    x - x3 / 6.0 + x5 / 120.0 - x7 / 5040.0
+}
+
+pub const fn rotation_x(r: f32) -> Matrix<4, 4> {
+    let mut m = Matrix::identity();
+    m.set(1, 1, cos(r));
+    m.set(1, 2, -1.0 * sin(r));
+
+    m.set(2, 1, sin(r));
+    m.set(2, 2, cos(r));
+    m
+}
+
+pub const fn rotation_y(r: f32) -> Matrix<4, 4> {
+    let mut m = Matrix::identity();
+    m.set(0, 0, cos(r));
+    m.set(0, 2, sin(r));
+
+    m.set(2, 0, -1.0 * sin(r));
+    m.set(2, 2, cos(r));
+    m
+}
+
+pub const fn rotation_z(r: f32) -> Matrix<4, 4> {
+    let mut m = Matrix::identity();
+    m.set(0, 0, cos(r));
+    m.set(0, 1, -1.0 * sin(r));
+
+    m.set(1, 0, sin(r));
+    m.set(1, 1, cos(r));
+    m
+}
+
+pub const fn shearing(x_y: f32, x_z: f32, y_x: f32, y_z: f32, z_x: f32, z_y: f32) -> Matrix<4, 4> {
+    let mut m = Matrix::identity();
+    m.set(0, 1, x_y);
+    m.set(0, 2, x_z);
+    m.set(1, 0, y_x);
+    m.set(1, 2, y_z);
+    m.set(2, 0, z_x);
+    m.set(2, 1, z_y);
+    m
+}
+
 #[test]
 fn multiplying_by_a_translation_matrix() {
     const TRANSFORM: Matrix<4, 4> = translation(5.0, -3.0, 2.0);
-    println!("{:#?}", TRANSFORM);
     let p = Tuple::point(-3.0, 4.0, 5.0);
     assert_eq!(TRANSFORM * p, Tuple::point(2.0, 1.0, 7.0));
 }
